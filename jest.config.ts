bda91489a5bd829f018308ab45a27eb048dcb21f
eldurynn <l.rose@misterti.com>
2025-03-29 16:27:56 +0100

    test: add specific configuration for jest

--- jest.config.ts
+++ jest.config.ts
@@ -4,4 +4,17 @@ import presets from 'jest-preset-angular/presets';
 export default {
   ...presets.createCjsPreset(),
   setupFilesAfterEnv: ['<rootDir>/setup-jest.ts'],
+  modulePathIgnorePatterns: ['<rootDir>/e2e-playwright'],
+  coverageThreshold: {
+    global: {
+      branches: 80,
+      functions: 80,
+      lines: 80,
+      statements: -10
+    }
+  },
+  displayName: {
+    name: 'TYRIA ANGULAR APP',
+    color: 'blue'
+  }
 } satisfies Config;
